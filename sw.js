0f37811594e78698be268b74e7d3fd8cae873bb4
kabweynew <kabweynew@gmail.com>
2026-03-18 08:37:05 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'hask-test-v1';
+const ASSETS = ['/OliTest/', '/OliTest/index.html', '/OliTest/manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).then(res => {
+    const clone = res.clone();
+    caches.open(CACHE).then(c => c.put(e.request, clone));
+    return res;
+  }).catch(() => caches.match(e.request)));
+});
